3dc67cd0ff41aa43ce37afac4b5c2a23530d2fc7
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-10 09:11:38 +0530

    Update Index

---
+++ src/lk/ijse/pos/filter/MyFilter.java
@@ -0,0 +1,29 @@
+package lk.ijse.pos.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : JavaEE POS System
+ * @date : 5/10/2022
+ * @month : 05
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+public class MyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
