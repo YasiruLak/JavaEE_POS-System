dda12d700f791e9847758fd1ecde734d4288a024
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-11 23:31:23 +0530

    Set Current Date

---
+++ src/lk/ijse/pos/servlet/OrderServlet.java
@@ -0,0 +1,14 @@
+package lk.ijse.pos.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : JavaEE POS System
+ * @date : 5/11/2022
+ * @month : 05
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+public class OrderServlet extends HttpServlet {
+}
---
+++ web/assests/script/OrderController.js
@@ -0,0 +1,11 @@
+function setCurrentDate(){
+    let orderDate = $('#orderDate');
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+    today = dd + '/' + mm + '/' + yyyy;
+    orderDate.val(today);
+}
+
+setCurrentDate();
