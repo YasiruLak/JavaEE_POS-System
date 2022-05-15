60009115e0d1c179c1380b4993bc6abb734357eb
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-15 20:09:27 +0530

    Add To Cart Function Done

--- src/lk/ijse/pos/servlet/OrderServlet.java
+++ src/lk/ijse/pos/servlet/OrderServlet.java
@@ -3,6 +3,7 @@ package lk.ijse.pos.servlet;
 import javax.annotation.Resource;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +13,6 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -34,22 +34,42 @@ public class OrderServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 
-//        try {
-//            resp.setContentType("application/json");
-//            Connection connection = dataSource.getConnection();
-//            PrintWriter writer = resp.getWriter();
-//
-//            resp.addHeader("Access-Control-Allow-Origin", "*");
-//
-//            ResultSet resultSet = connection.prepareStatement("SELECT * FROM Customer").executeQuery();
-//            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
-//
-//            while (resultSet.next()){
-//
-//            }
+        try {
+            resp.setContentType("application/json");
+            Connection connection = dataSource.getConnection();
+            PrintWriter writer = resp.getWriter();
 
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+            resp.addHeader("Access-Control-Allow-Origin", "*");
+
+            ResultSet resultSet = connection.prepareStatement("SELECT * FROM Orders").executeQuery();
+            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+
+            while (resultSet.next()){
+                String oId = resultSet.getString(1);
+                String cId = resultSet.getString(2);
+                String date = resultSet.getString(3);
+                String time = resultSet.getString(4);
+                String total = resultSet.getString(5);
+
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                objectBuilder.add("orderId",oId);
+                objectBuilder.add("cid",cId);
+                objectBuilder.add("orderDate",date);
+                objectBuilder.add("time",time);
+                objectBuilder.add("total",total);
+                arrayBuilder.add(objectBuilder.build());
+            }
+
+            JsonObjectBuilder response = Json.createObjectBuilder();
+            response.add("status", 200);
+            response.add("message", "Done");
+            response.add("data", arrayBuilder.build());
+            writer.print(response.build());
+
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- web/assests/script/OrderController.js
+++ web/assests/script/OrderController.js
@@ -1,4 +1,29 @@
+function generateOrderID() {
+    $("#txtOrderID").val("O00-0001");
+    $.ajax({
+        url: "order",
+        method: "GET",
+        success: function (resp) {
+            for (const orders of resp.data){
+                let orderId = orders.oId;
+                let tempId = parseInt(orderId.split("-")[1]);
+                tempId = tempId+1;
+                if (tempId <= 9){
+                    $("#txtOrderID").val("O00-000"+tempId);
+                }else if (tempId <= 99) {
+                    $("#txtOrderID").val("O00-00" + tempId);
+                }else if (tempId <= 999){
+                    $("#txtOrderID").val("O00-0" + tempId);
+                }else {
+                    $("#txtOrderID").val("O00-"+tempId);
+                }
+            }
 
+        }
+    });
+}
+
+generateOrderID();
 
 function setCurrentDate(){
     let orderDate = $('#txtOrderDate');
@@ -6,7 +31,7 @@ function setCurrentDate(){
     let dd = String(today.getDate()).padStart(2, '0');
     let mm = String(today.getMonth() + 1).padStart(2, '0');
     let yyyy = today.getFullYear();
-    today = dd + '/' + mm + '/' + yyyy;
+    today = yyyy + '-' + mm + '-' + dd;
     orderDate.val(today);
 }
 
@@ -78,3 +103,107 @@ $("#txtOrderItemCode").click(function (){
         }
     });
 });
+
+var tableRow;
+
+$("#btnAddToCart").click(function () {
+    let duplicate = false;
+
+    for (let i = 0; i < $("#addToCartTable tr").length; i++) {
+        if($("#txtOrderItemCode option:selected").text() == $("#addToCartTable tr").children(':nth-child(1)')[i].innerText){
+            duplicate = true;
+        }
+    }
+
+    if (duplicate != true){
+        loadOrderDetail();
+        minusQty($("#txtQty").val());
+        manageTotal($("#txtQty").val() * $("#txtOrderItemPrice").val());
+
+    }else if (duplicate == true){
+        manageQuantity(tableRow.children(':nth-child(4)').text(),$("#txtQty").val());
+        $(tableRow).children(':nth-child(4)').text($("#txtQty").val());
+
+        updateManageTotal(tableRow.children(':nth-child(5)').text(),$("#txtQty").val() * $("#txtOrderItemPrice").val());
+        $(tableRow).children(':nth-child(5)').text($("#txtQty").val() * $("#txtOrderItemPrice").val());
+    }
+
+    $("#addToCartTable>tr").click('click',function () {
+
+        tableRow = $(this);
+        let itemCode = $(this).children(":eq(0)").text();
+        let itemName = $(this).children(":eq(1)").text();
+        let unitPrice = $(this).children(":eq(2)").text();
+        let qty = $(this).children(":eq(3)").text();
+        let total = $(this).children(":eq(4)").text();
+
+        $("#txtOrderItemCode").val(itemCode);
+        $("#txtOrderItemName").val(itemName);
+        $("#txtOrderItemPrice").val(unitPrice);
+        $("#txtQty").val(qty);
+
+    });
+
+});
+
+var itemCode;
+var itemName;
+var itemPrice;
+var itemQty;
+var itemOrderQty;
+
+$("#addToCartTable").empty();
+function loadOrderDetail() {
+
+    itemCode = $("#txtOrderItemCode").val();
+    itemName = $("#txtOrderItemName").val();
+    itemPrice = $("#txtOrderItemPrice").val();
+    itemQty = $("#txtOrderItemQtyOnHand").val();
+    itemOrderQty = $("#txtQty").val();
+
+
+    let total;
+
+    total = itemPrice * itemOrderQty;
+
+    $("#addToCartTable").append("<tr>" +
+        "<td>" + itemCode + "</td>" +
+        "<td>" + itemName + "</td>" +
+        "<td>" + itemPrice + "</td>" +
+        "<td>" + itemOrderQty + "</td>" +
+        "<td>" + total + "</td>" +
+        "</tr>");
+}
+
+function minusQty(orderQty){
+    var minusQty = parseInt(orderQty);
+    var manageQty = parseInt($("#txtOrderItemQtyOnHand").val());
+
+    manageQty = manageQty - minusQty;
+
+    $("#txtOrderItemQtyOnHand").val(manageQty);
+}
+
+var total = 0;
+function manageTotal(amount){
+    total = amount;
+    $("#total").val(total);
+}
+
+function updateManageTotal(prvTotal,nowTotal) {
+    total -= prvTotal;
+    total += nowTotal;
+
+    $("#total").val(total);
+}
+
+function manageQuantity(prevQty,nowQty){
+    var prevQty = parseInt(prevQty);
+    var nowQty = parseInt(nowQty);
+    var availableQty = parseInt($("#txtOrderItemQtyOnHand").val());
+
+    availableQty = availableQty + prevQty;
+    availableQty = availableQty - nowQty;
+
+    $("#txtOrderItemQtyOnHand").val(availableQty);
+}
