9441edb7fd8bbb3971f4f9183ba8ad9337bf80e0
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-17 22:42:20 +0530

    Create DB Connection

--- src/lk/ijse/pos/db/DBConnection.java
+++ src/lk/ijse/pos/db/DBConnection.java
@@ -1,5 +1,9 @@
 package lk.ijse.pos.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author : Yasiru Dahanayaka
  * @name : JavaEE POS System
@@ -9,4 +13,22 @@ package lk.ijse.pos.db;
  * @since : 0.1.0
  **/
 public class DBConnection {
+
+    private static DBConnection dbConnection;
+    private final Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaeepos",
+                "root",
+                "19980611");
+    }
+
+    public static DBConnection getDbConnection() throws SQLException, ClassNotFoundException {
+        return dbConnection == null? dbConnection = new DBConnection() : dbConnection;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
 }
