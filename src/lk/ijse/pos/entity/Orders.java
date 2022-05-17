8af523458b51ba25412295a90f909bd670073a69
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-05-17 19:43:30 +0530

    Orders Entity Create

---
+++ src/lk/ijse/pos/entity/Orders.java
@@ -0,0 +1,72 @@
+package lk.ijse.pos.entity;
+
+import java.sql.Date;
+import java.sql.Time;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : JavaEE POS System
+ * @date : 5/17/2022
+ * @month : 05
+ * @year : 2022
+ * @since : 0.1.0
+ **/
+public class Orders {
+
+    private String orderId;
+    private String cId;
+    private Date orderDate;
+    private Time time;
+    private double total;
+
+    public Orders() {
+    }
+
+    public Orders(String orderId, String cId, Date orderDate, Time time, double total) {
+        this.orderId = orderId;
+        this.cId = cId;
+        this.orderDate = orderDate;
+        this.time = time;
+        this.total = total;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getcId() {
+        return cId;
+    }
+
+    public void setcId(String cId) {
+        this.cId = cId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
